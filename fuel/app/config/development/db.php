24ec118156e4060b3eb0cafdc15eb70f1f8c679c
sand <renarika2@gmail.com>
2013-04-13 16:40:15 +0900

    change dbhost

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuelhandson',
+			'dsn'        => 'mysql:host=127.0.0.1;dbname=fuelhandson',
 			'username'   => 'fuelhandson',
 			'password'   => '',
 		),
